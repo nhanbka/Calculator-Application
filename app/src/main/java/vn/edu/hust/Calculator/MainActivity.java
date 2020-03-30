4fb5dfad270fee8898bee833391a25fc7646f0c9
NhanBKA <nhanletrong999@gmail.com>
2020-03-30 23:58:37 +0700

    Upload Project

---
+++ app/src/main/java/vn/edu/hust/Calculator/MainActivity.java
@@ -0,0 +1,38 @@
+package vn.edu.hust.Calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtDisplay;
+    private Button btnDivide;
+    private Button btnMultiply;
+    private Button btnMinus;
+    private Button btnAdd;
+    private Button btnClearUnitElement;
+    private Button Delete;
+    private Button btnClearElement;
+    private Button btnClear;
+    private Button btnDelete;
+    private Button btnZero;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
